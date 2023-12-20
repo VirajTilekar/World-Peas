c0717cbe78a99b2f3fe4dd98e468c75bfac2b00c
Viraj Tilekar <67374433+VirajTilekar@users.noreply.github.com>
2023-12-20 18:59:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'accent':'#426B1F',
+        'card':'#FAFAF5'
+      },
+      fontFamily: {
+        'news': ['Newsreader'],
+        'inter': ['Inter'],
+        'poppins':['Poppins'],
+      },
+    },
+  },
+  plugins: [],
+}
+
